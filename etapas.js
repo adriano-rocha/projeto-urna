b2cde3b271a5791e049923a5f211ac0554ef07f2
adriano-rocha <adrianorocha.dev@gmail.com>
2023-11-24 08:25:41 -0300

    projeto urna concluído!

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '40',
+                nome: 'Carlos Massa (Ratinho)',
+                partido: 'SBT',
+                fotos:[
+                    {url:'ratinho.jpg', legenda: 'Governador'}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Tiririca',
+                partido: 'PSK',
+                fotos:[
+                    {url:'tiririca.jpg', legenda: 'Governador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '24',
+                nome: 'Gretchen',
+                partido: 'PPQ',
+                vice: 'Amin Khader',
+                fotos:[
+                    {url:'gretchen.jpg', legenda: 'Presidente'},
+                    {url:'amin-khader.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Abel Ferreira',
+                partido: 'SEP',
+                vice: 'Dr.Lair Ribeiro',
+                fotos:[
+                    {url:'abel.jpg', legenda: 'Presidente'},
+                    {url:'lair ribeiro.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
